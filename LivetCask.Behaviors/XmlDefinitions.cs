2c369ccc74b754309ba52dda437dc032a74feabd
kttFox <kttFoxOnFire@gmail.com>
2026-02-03 01:17:25 +0900

    [feat] Remove Generator

--- LivetCask.Behaviors/XmlDefinitions.cs
+++ LivetCask.Behaviors/XmlDefinitions.cs
@@ -3,4 +3,3 @@
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors.ControlBinding")]
-[assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors.ControlBinding.OneWay")]
