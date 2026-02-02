488b4d2e4a04a1cefd9cedcf315580f49f07ea18
kttFox <kttFoxOnFire@gmail.com>
2026-02-03 01:17:25 +0900

    [feat] localize templates for Japanese

--- Templates/ItemTemplates/Sylpha.ViewModel/ViewModel.cs
+++ Templates/ItemTemplates/Sylpha.ViewModel/ViewModel.cs
@@ -7,17 +7,11 @@ using System.ComponentModel;
 using Sylpha;
 using Sylpha.Commands;
 using Sylpha.Messaging;
-using Sylpha.Messaging.IO;
-using Sylpha.EventListeners;
-using Sylpha.Messaging.Windows;
 
 namespace $rootnamespace$
 {
     public class $safeitemname$ : ViewModel
     {
-        // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
-
-        // This method would be called from View, when ContentRendered event was raised.
         public void Initialize()
         {
         }
--- Templates/Sylpha.ProjectTemplate/ViewModels/MainWindowViewModel.cs
+++ Templates/Sylpha.ProjectTemplate/ViewModels/MainWindowViewModel.cs
@@ -15,7 +15,6 @@ namespace $safeprojectname$.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
-        // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
         public void Initialize()
         {
         }
