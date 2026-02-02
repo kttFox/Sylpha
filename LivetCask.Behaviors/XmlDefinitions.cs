6d5a0431bb1354a5bc2718bb46018cbdde144b77
kttFox <kttFoxOnFire@gmail.com>
2026-02-03 01:17:25 +0900

    [feat] Remove LivetCask.Behaviors

--- LivetCask.Behaviors/XmlDefinitions.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Windows.Markup;
-
-[assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet")]
-[assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors")]
-[assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors.ControlBinding")]
--- LivetCask.Messaging/XmlDefinitions.cs
+++ LivetCask.Messaging/XmlDefinitions.cs
@@ -3,6 +3,7 @@
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Messaging")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Messaging.IO")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Messaging.Windows")]
+[assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors.Messaging")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors.Messaging.IO")]
 [assembly: XmlnsDefinition("http://schemas.livet-mvvm.net/2011/wpf", "Livet.Behaviors.Messaging.Windows")]
