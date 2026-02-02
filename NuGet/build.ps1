0329345b66e28a347c71d38992b33424fdf8e210
kttFox <kttFoxOnFire@gmail.com>
2026-02-03 01:17:25 +0900

    [refactor] update NuGet

---
+++ NuGet/build.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+
+# Sylpha.props から Version を取得
+$doc = [XML](Get-Content "..\Sylpha.props")
+$Version = $doc.Project.PropertyGroup.Version
+
+Write-Host "Building version: $Version"
+
+dotnet build ..\Sylpha.Code.slnx -c Release
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+$distPath = "dist\$Version"
+New-Item -ItemType Directory -Path $distPath -Force | Out-Null
+
+$packages = @(
+    "Sylpha",
+    "Sylpha.EventListeners",
+    "Sylpha.Messaging",
+    "Sylpha.Messaging.Extensions"
+)
+
+foreach ($pkg in $packages) {
+    Copy-Item "..\$pkg\bin\Release\$pkg.$Version.nupkg" $distPath
+}
+
+Write-Host "Packages copied to $distPath"
---
+++ NuGet/push.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+
+# Sylpha.props から Version を取得
+$doc = [XML](Get-Content "..\Sylpha.props")
+$Version = $doc.Project.PropertyGroup.Version
+
+$DistPath = ".\dist\$Version"
+
+# Please get an API Key from nuget.org, and set the key using: dotnet nuget setApiKey xxxx
+Get-ChildItem -Path $DistPath -Filter "*.nupkg" | ForEach-Object {
+    Write-Host "Pushing $($_.Name)..." -ForegroundColor Cyan
+    dotnet nuget push $_.FullName -s https://www.nuget.org/api/v2/package
+}
+
